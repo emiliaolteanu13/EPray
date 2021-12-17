2f7e5eb1884c843c088cc5e13a423bd765bad944
emiliaolteanu13 <emiliaolteanu13@yahoo.com>
2021-12-17 12:57:55 +0200

    register page

---
+++ EPray/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPray.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ EPray/Models/RegistrationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPray.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Character limit overflow!")]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email Address!")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
