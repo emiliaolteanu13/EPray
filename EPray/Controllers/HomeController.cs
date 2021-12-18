a2e0488501fdd6810dbc231a4ef2237103b44f96
NastyTester <70013669+NastyTester@users.noreply.github.com>
2021-12-18 16:12:53 +0200

    added donation page

--- EPray/Controllers/HomeController.cs
+++ EPray/Controllers/HomeController.cs
@@ -50,6 +50,11 @@ namespace EPray.Controllers
             return View();
         }
 
+        public IActionResult Donations()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
